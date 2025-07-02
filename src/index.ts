4b95aebbda5b2e8fdd50defb852deb4b1152576c
nantasit <nantasitnsboss@gmail.com>
2025-07-02 17:30:19 +0700

    feat: sync routes and clekrk

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import Fastify from 'fastify';
+import cors from '@fastify/cors';
+import roomsRoutes from './routes/rooms';
+import adminRoomRoutes from './routes/admin/rooms';
+import clerkWebhook from './routes/clerkWebhook';
+const fastify = Fastify({ logger: true });
+
+const start = async () => {
+  await fastify.register(cors, {
+    origin: '*',
+  });
+  fastify.register(clerkWebhook, { prefix: '/webhooks' });
+  fastify.register(roomsRoutes, { prefix: '/rooms' });
+  fastify.register(adminRoomRoutes, { prefix: '/admin/rooms' });
+  try {
+    await fastify.listen({ port: 3001, host: '0.0.0.0' });
+    console.log('Server running on http://localhost:3001');
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
